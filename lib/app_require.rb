6e0e7ff05fd12f359fa98dc6707f9aade3b8f083
Jefferson Barbosa <jefferson.dab@gmail.com>
2022-10-25 23:21:46 -0300

    Adding i18n support

--- lib/app_require.rb
+++ lib/app_require.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'discordrb'
+require 'i18n'
+
+require_relative '../config/initializers/i18n'
 
 require_relative 'valobot/commands'
 require_relative 'valobot/commands/abstract_command'
